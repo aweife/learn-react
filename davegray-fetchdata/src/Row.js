c879b95a48f2c121d72c91f9ac0165b30ff19659
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2023-02-23 21:47:17 +0800

    add completed tutorials

---
+++ davegray-fetchdata/src/Row.js
@@ -0,0 +1,14 @@
+import Cell from "./Cell"
+
+const Row = ({ item }) => {
+    return (
+        <tr>
+            {Object.entries(item).map(([key, val]) => {
+                return (
+                    <Cell key={key} cellData={JSON.stringify(val)} />
+                )
+            })}
+        </tr>
+    )
+}
+export default Row
---
+++ davegray-grocerylist/src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = ({ length }) => {
+    // const today = new Date();
+
+    return (
+        <footer>
+            {/* <p>Copyright &copy; {today.getFullYear()}</p> */}
+            <p>{length} list {length === 1 ? "item" : "items"}</p>
+        </footer>
+    )
+}
+export default Footer
