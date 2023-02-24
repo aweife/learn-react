284212a3326476bb87f1d1d95fb301e76572ae86
Ang Wei Feng (Ted) <aweifeis@gmail.com>
2023-02-24 21:31:11 +0800

    19tut use axios api

--- davegray-blog/src/App.js
+++ davegray-blog/src/App.js
@@ -2,6 +2,8 @@ import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import About from './About';
+import api from './api/posts';
+import EditPost from './EditPost';
 import Home from './Home';
 import Layout from './Layout';
 import Missing from './Missing';
@@ -14,56 +16,119 @@ function App() {
     const [searchResults, setSearchResults] = useState([]);
     const [postTitle, setPostTitle] = useState("");
     const [postBody, setPostBody] = useState("");
+    const [editTitle, setEditTitle] = useState("");
+    const [editBody, setEditBody] = useState("");
 
+    // fetch data
     useEffect(() => {
-        const filteredResults = posts.filter(post =>
+        const fetchPosts = async () => {
+            try {
+                const response = await api.get("/posts");
+                setPosts(response.data);
+            } catch (err) {
+                if (err.response) {
+                    // not in the 2xx response code range
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                } else {
+                    console.log(`Error: ${err.message}`);
+                }
+            }
+        }
+
+        fetchPosts();
+    }, []);
+
+    // typing in search bar
+    useEffect(() => {
+        const filteredResults = posts.filter((post) =>
             ((post.body).toLowerCase()).includes(search.toLowerCase()) ||
             ((post.title).toLowerCase()).includes(search.toLowerCase()));
 
         setSearchResults(filteredResults.reverse());
-    }, [posts, search])
+    }, [posts, search]);
 
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
         const datetime = format(new Date(), "MMMM dd, yyyy pp");
         const newPost = { id, title: postTitle, datetime, body: postBody };
-        const allPosts = [...posts, newPost];
-        setPosts(allPosts);
+        try {
+            const response = await api.post("/posts", newPost);
+            const allPosts = [...posts, response.data];
+            setPosts(allPosts);
 
-        setPostTitle("");
-        setPostBody("");
-        navigate("/");
-    }
+            // reset state
+            setPostTitle("");
+            setPostBody("");
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
 
-    const handleDelete = (id) => {
-        const postsList = posts.filter(post => post.id !== id);
-        setPosts(postsList);
-        navigate("/");
+    };
+
+    const handleEdit = async (id) => {
+        const datetime = format(new Date(), "MMMM dd, yyyy pp");
+        const updatedPost = { id, title: editTitle, datetime, body: editBody };
+        try {
+            const response = await api.put(`/posts/${id}`, updatedPost);
+            setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+
+            // reset state
+            setEditTitle("");
+            setEditBody("");
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/posts/${id}`);
+            const postsList = posts.filter(post => post.id !== id);
+            setPosts(postsList);
+            navigate("/");
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
     };
 
     return (
         <Routes>
-            <Route path='/' element={<Layout
-                search={search}
-                setSearch={setSearch}
-            />}>
+            <Route path='/' element={<Layout search={search} setSearch={setSearch} />}>
                 <Route index element={<Home posts={searchResults} />} />
                 <Route path='post'>
-                    <Route index element={<NewPost
-                        postTitle={postTitle}
-                        setPostTitle={setPostTitle}
-                        postBody={postBody}
-                        setPostBody={setPostBody}
-                        handleSubmit={handleSubmit}
-                    />} />
-                    <Route path=':id' element={<PostPage
-                        posts={posts}
-                        handleDelete={handleDelete}
-                    />} />
+                    <Route index element={
+                        <NewPost
+                            handleSubmit={handleSubmit}
+                            postTitle={postTitle}
+                            setPostTitle={setPostTitle}
+                            postBody={postBody}
+                            setPostBody={setPostBody}
+                        />}
+                    />
+                    <Route path=':id' element={
+                        <PostPage
+                            posts={posts}
+                            handleDelete={handleDelete}
+                        />}
+                    />
                 </Route>
+                <Route path='edit/:id' element={
+                    <EditPost
+                        posts={posts}
+                        handleEdit={handleEdit}
+                        editTitle={editTitle}
+                        setEditTitle={setEditTitle}
+                        editBody={editBody}
+                        setEditBody={setEditBody}
+                    />}
+                />
                 <Route path='about' element={<About />} />
                 <Route path='*' element={<Missing />} />
             </Route>
